fbf57d5a568e447ed185abbd4da4f976f7fb8fc8
Jeremie Dayan <dayanjeremie@gmail.com>
2024-12-13 15:14:17 +0100

    codegen

---
+++ packages/domain/src/index.ts
@@ -0,0 +1,1 @@
+export * as TodosApi from "./TodosApi.js"
---
+++ packages/server/src/index.ts
@@ -0,0 +1,5 @@
+export * as Api from "./Api.js"
+
+export * as TodosRepository from "./TodosRepository.js"
+
+export * as server from "./server.js"
